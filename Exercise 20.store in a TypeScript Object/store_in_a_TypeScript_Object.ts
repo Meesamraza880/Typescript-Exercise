4eb2b542a95d46b0ae4eb485fb38cac2869e4010
Meesam Raza <118877259+Meesamraza880@users.noreply.github.com>
2023-05-09 17:56:38 +0500

    first assignment

---
+++ Exercise 20.store in a TypeScript Object/store_in_a_TypeScript_Object.ts
@@ -0,0 +1,14 @@
+const cultures = {
+  ar: "Arabic",
+  en: "English",
+  es: "spanish",
+  fr: "french",
+  hi: "Hindi",
+  ja: "japanese",
+  ko: "Korean",
+  pt: "Portuguese",
+  ru: "Russian",
+  zh: "Chinese",
+};
+
+console.log(cultures);
